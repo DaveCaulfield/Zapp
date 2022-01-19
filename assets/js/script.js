c1c3b849623af092680317f3791f1c8a3243201e
DaveCaulfield <dave.caulfield@gmail.com>
2022-01-19 19:44:04 +0000

    Add a loop count to end into the game loop

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,30 @@
-
+/*Add event listener to play button to call startGame function*/
 document.getElementById("play-btn").addEventListener("click", startGame);
 
+/* Start Game function invokes the game loop*/
 function startGame() {
     gameLoop();
 }
 
+/* Game loop function - loops through a loop count to end*/
+let loopCount = 0;
 function gameLoop(){
-    alert("mow we're running the game loop")
+    loopCount++;
+    if(loopCount < 4){
+    alert("mow we're running the  loop count ")
+    setTimeout(gameLoop, 2000);
+}
+else {
+    alert("game over");
+}
 }
 
+
+
+
+
+
+/*
 function createSpacehips
 
 
@@ -17,3 +33,4 @@ function flashSpaceships
 function incrementScore
 
 function countdownTimer
+*/
