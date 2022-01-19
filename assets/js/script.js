43a60329f3773c3c2ecfd1873133b607684d6123
DaveCaulfield <dave.caulfield@gmail.com>
2022-01-19 20:59:11 +0000

    Add basic image style to spaceship divs

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,18 +6,34 @@ function startGame() {
     gameLoop();
 }
 
-/* Game loop function - loops through a loop count to end*/
+/* Game loop function:
+loops through a loop count
+calls the flashing spaceship function*/
 let loopCount = 0;
+let spaceshipVisible = false;
 function gameLoop(){
+    spaceshipVisible = !spaceshipVisible;
+    flashingSpaceships();
     loopCount++;
-    if(loopCount < 4){
-    alert("mow we're running the  loop count ")
-    setTimeout(gameLoop, 2000);
+    if(loopCount < 12){
+    setTimeout(gameLoop, 1000);
 }
 else {
     alert("game over");
 }
 }
+/*flashing spaceship function*/
+function flashingSpaceships(){
+    let gameArea = document.getElementById("game-area");
+    let setClass = spaceshipVisible ? "spaceship visible" : "spaceship invisible";
+    
+    for (let i=0; i < 8; i++) {
+        gameArea.children[i].className = setClass;
+    }
+}
+
+
+
 
 
 
