bd36b16f65f65c366caf831081ee612c2edb27a3
DaveCaulfield <dave.caulfield@gmail.com>
2022-01-20 12:42:01 +0000

    Add flight formation function

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,6 +22,29 @@ else {
     alert("game over");
 }
 }
+
+
+/*  
+flight formation function.
+set the game-area to ufoArea
+loop through the child divs for every i less than 8
+set the child divs innerHTML to "Alien"
+set randomUfo to a random number between 1-8
+(use random-1 to translate 1-8 into index count 0-7)
+set the child divs innerHTML to "Rogue UFO"
+*/
+function flightFormation() {
+    let  ufoArea= document.getElementById("game-area");
+    for(let i=0; i < 8; i++){
+        ufoArea.children[i].innerHTML = "Alien";
+    }
+    let randomUfo = Math.floor(Math.random() * 8) + 1;
+    ufoArea.children[randomUfo-1].innerHTML = "Rogue UFO";
+}
+
+
+
+
 /*flashing spaceship function*/
 function flashingSpaceships(){
     let gameArea = document.getElementById("game-area");
