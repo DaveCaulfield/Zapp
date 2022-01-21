cceca9e9e89a40fbe1004e691a44b987463a93e9
DaveCaulfield <dave.caulfield@gmail.com>
2022-01-21 16:52:11 +0000

    Add sound effect feature for hiting ufo

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,10 +38,6 @@ else {
 
 }/*...closing bracket game loop...........*/
 
-function playStart() {
-    var audioStart = document.getElementById("audio-start");
-    audioStart.play();
-  }
 
 /*  
 spaceShips function:
@@ -63,16 +59,22 @@ function spaceships() {
             document.getElementById("player-score").innerText = "Crypto Coins: " + score * 1000;
         }   
     }
-
     let randomUfo = Math.floor(Math.random() * 8) + 1;
     ufoArea.children[randomUfo-1].innerHTML = "ROGUE-UFO";
    
     ufoArea.children[randomUfo-1].onclick = function(){
         score++;
         document.getElementById("player-score").innerText = "Crypto Coins: " + score * 1000;
+        playUfoSound();
     } 
 }
 
+
+function playUfoSound() {
+    var audioUfo = document.getElementById("audio-ufo");
+    audioUfo.play();
+  }
+  
 /*flashing spaceship function
 set id game-area as gameArea.
 set class for CSS .visible or .invisible. Depends if spaceshipVisible is true or false.
@@ -87,16 +89,16 @@ function flashingSpaceships(){
     }
 }
 
+function playStart() {
+    var audioStart = document.getElementById("audio-start");
+    audioStart.play();
+  }
 
+ 
 
 
 /*
-function createSpacehips
 
-
-function flashSpaceships
-
-function incrementScore
 
 function countdownTimer
 */
