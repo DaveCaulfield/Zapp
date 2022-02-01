202c309332e282a0d135cff803ba5b34f8ce7287
DaveCaulfield <dave.caulfield@gmail.com>
2022-02-01 10:01:27 +0000

    Add modal feature to display score results when game finishes

--- assets/js/script.js
+++ assets/js/script.js
@@ -66,8 +66,9 @@ function gameLoop() {
     setTimeout(gameLoop, spaceshipVisible ? 1000 : 1800);
   } else {
     playGameOver();
-    alert("you scored " + score * 1000 + " Crypto Coins");
-    window.location.reload();
+    scoreModal();
+    // alert("you scored " + score * 1000 + " Crypto Coins");
+    // window.location.reload();
   }
 }
 
@@ -195,3 +196,58 @@ function disableMute() {
   audioAlien.muted = false;
   audioGameOver.muted = false;
 }
+
+
+
+
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+// btn.onclick = function() {
+  function scoreModal(){
+  modal.style.display = "block";
+  if(score < 1){
+    document.getElementById("score-result").innerHTML = "What happened out there? your zapp balance is " + score * 1000 + " crypto coins!!";
+  } else if (score > 20){
+    document.getElementById("score-result").innerHTML = "Nailed it! " + score * 1000 + " crypto coins in the bag!";
+  } else if (score > 15){
+    document.getElementById("score-result").innerHTML = "Sharp shooting out there!, you just scored " + score * 1000 + " crypto coins!!";
+  } else if (score > 10){
+    document.getElementById("score-result").innerHTML = "Nice work, you zapped well, you scored " + score * 1000 + " crypto coins!!";
+  } else if (score > 5){
+    document.getElementById("score-result").innerHTML = "OK you're warming up " + score * 1000 + " crypto coins!!";
+  } else if (score >= 1 && score <= 5){
+    document.getElementById("score-result").innerHTML = "Not bad, you scored " + score * 1000 + " crypto coins!!";
+  } else {
+    document.getElementById("score-result").innerHTML = "Congratulations " + score * 1000 + " crypto coins!!";
+  }
+ 
+}
+
+
+
+
+
+// When the user clicks on <span> (x), close the modal
+if(span){
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+  }
+  
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+      window.location.reload();
+    }
+  }
